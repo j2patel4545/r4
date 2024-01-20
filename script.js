dfa42915d28b66eb853a63a7b97b3f1bd8955cde
jetal kalpanaben satishbhai patel <145288109+j2patel4545@users.noreply.github.com>
2024-01-20 09:53:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var x = document.getElementById("myAudio").autoplay;
+    document.getElementById("demo").innerHTML = x;
+  }
+
+//   const scroll = new LocomotiveScroll({
+//     el: document.querySelector('body'),
+//     smooth: true
+// });
+
+
